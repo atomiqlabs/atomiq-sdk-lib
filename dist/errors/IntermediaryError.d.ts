7b13cc32d546b12b11aa218271479c46fbd3b19e
adambor <adamborteam@gmail.com>
2025-08-28 17:42:11 +0200

    Retry when LP returns already spent UTXO for spv vault swap

--- dist/errors/IntermediaryError.d.ts
+++ dist/errors/IntermediaryError.d.ts
@@ -2,5 +2,6 @@
  * An error or inconsistency in the intermediary's returned data, this will blacklist the intermediary
  */
 export declare class IntermediaryError extends Error {
-    constructor(msg: string);
+    recoverable: boolean;
+    constructor(msg: string, recoverable?: boolean);
 }
