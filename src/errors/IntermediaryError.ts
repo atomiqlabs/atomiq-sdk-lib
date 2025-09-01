9bc0f855ee1ca31ceec06a5386be5ddafbec95b0
adambor <adamborteam@gmail.com>
2025-09-01 09:53:16 +0200

    Add better error reporting to IntermediaryError-s

--- src/errors/IntermediaryError.ts
+++ src/errors/IntermediaryError.ts
@@ -4,9 +4,17 @@
 export class IntermediaryError extends Error {
 
     recoverable: boolean;
+    originalStack?: string;
 
-    constructor(msg: string, recoverable: boolean = false) {
+    constructor(msg: string, originalError?: any, recoverable: boolean = false) {
+        if(originalError!=null) {
+            if(originalError.name!=null) msg += ": "+originalError.name;
+            if(originalError.message!=null) msg += ": "+originalError.message;
+            if(typeof(originalError)==="string") msg += ": "+originalError;
+        }
         super(msg);
+
+        if(originalError?.stack!=null) this.originalStack = originalError.stack;
         this.recoverable = recoverable;
         // Set the prototype explicitly.
         Object.setPrototypeOf(this, IntermediaryError.prototype);
