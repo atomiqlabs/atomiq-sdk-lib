3c68fe4c84e818f69618c0555d84b3c32066d1f6
adambor <adamborteam@gmail.com>
2025-09-22 10:42:25 +0200

    Add .execute() helper to legacy swaps

--- src/swaps/escrow_swaps/frombtc/ln_auto/FromBTCLNAutoSwap.ts
+++ src/swaps/escrow_swaps/frombtc/ln_auto/FromBTCLNAutoSwap.ts
@@ -395,7 +395,7 @@ export class FromBTCLNAutoSwap<T extends ChainType = ChainType>
     //// Execution
 
     /**
-     * Executes the swap with the provided bitcoin lightning network wallet
+     * Executes the swap with the provided bitcoin lightning network wallet or LNURL
      *
      * @param walletOrLnurlWithdraw Bitcoin lightning wallet to use to pay the lightning network invoice, or an LNURL-withdraw
      *  link, if the quote was created using LNURL-withdraw you don't need to pass any wallet or lnurl
@@ -412,7 +412,6 @@ export class FromBTCLNAutoSwap<T extends ChainType = ChainType>
             onSwapSettled?: (destinationTxId: string) => void
         },
         options?: {
-            feeRate?: number,
             abortSignal?: AbortSignal,
             lightningTxCheckIntervalSeconds?: number,
             maxWaitTillAutomaticSettlementSeconds?: number
